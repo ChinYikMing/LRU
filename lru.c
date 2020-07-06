e333caf4d3469ffc06f9540e3f6ff27f244f42e2
ChinYikMing <58044437+ChinYikMing@users.noreply.github.com>
2020-07-06 16:21:04 +0800

    Update lru.c

--- lru.c
+++ lru.c
@@ -1,7 +1,7 @@
 #include "lru.h"
 
 int LRUCache_init(LRUCache *cache, size_t cap_bits){
-    int ret = map_init(cache->map, cap_bits);
+    int ret = map_init(&cache->map, cap_bits);
     if(ret < 0)
         return -1;
 
@@ -9,14 +9,14 @@ int LRUCache_init(LRUCache *cache, size_t cap_bits){
 }
 
 int LRUCache_get(LRUCache *cache, char *key){
-    map_get(cache->map, key);
+    map_get(&cache->map, key);
 
-    printf("The most used is %s\n", cache->map->front->key);
+    printf("The most used is %s\n", cache->map.front->key);
     return 0;
 }
 
 int LRUCache_destruct(LRUCache *cache){
-    map_destruct(cache->map);
+    map_destruct(&cache->map);
 
     return 0;
 }
