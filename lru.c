8d40bc7e17ea773660efaaaf3fe1b82e9bdd59a8
ChinYikMing <58044437+ChinYikMing@users.noreply.github.com>
2020-07-06 15:47:58 +0800

    Create lru.c

---
+++ lru.c
@@ -0,0 +1,22 @@
+#include "lru.h"
+
+int LRUCache_init(LRUCache *cache, size_t cap_bits){
+    int ret = map_init(cache->map, cap_bits);
+    if(ret < 0)
+        return -1;
+
+    return 0;
+}
+
+int LRUCache_get(LRUCache *cache, char *key){
+    map_get(cache->map, key);
+
+    printf("The most used is %s\n", cache->map->front->key);
+    return 0;
+}
+
+int LRUCache_destruct(LRUCache *cache){
+    map_destruct(cache->map);
+
+    return 0;
+}
