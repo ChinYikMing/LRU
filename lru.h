ab8ad7d2d786a86496b33314969c0d779f21c480
ChinYikMing <58044437+ChinYikMing@users.noreply.github.com>
2020-07-06 15:47:41 +0800

    Create lru.h

---
+++ lru.h
@@ -0,0 +1,19 @@
+
+#ifndef LRU_LRU_H
+#define LRU_LRU_H
+
+#include "hashmap.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef  struct lru_cache {
+    HashMap *map;
+} LRUCache;
+
+int LRUCache_init(LRUCache *cache, size_t cap_bits);
+
+int LRUCache_get(LRUCache *cache, char *key);
+
+int LRUCache_destruct(LRUCache *cache);
+
+#endif //LRU_LRU_H
